a8027e4e140ea5957f83d53771661286aa7fef5a
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-07-01 22:34:48 -0700

    Updates

---
+++ precache-manifest.98abc36c70b8946c1f8114fe3793cf17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a93c0276f0f9d66a807",
+    "url": "/Todo-App/static/css/main.ab0e9b97.chunk.css"
+  },
+  {
+    "revision": "2a93c0276f0f9d66a807",
+    "url": "/Todo-App/static/js/main.40bdd6fd.chunk.js"
+  },
+  {
+    "revision": "2acd21dc4a6582beab29",
+    "url": "/Todo-App/static/js/runtime~main.3fb21290.js"
+  },
+  {
+    "revision": "cd0478dde27931c233ea",
+    "url": "/Todo-App/static/js/2.fd99081e.chunk.js"
+  },
+  {
+    "revision": "c8a4785c310ea873a4f4318db96bf242",
+    "url": "/Todo-App/index.html"
+  }
+];
